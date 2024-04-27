df8182f3b938d0de19173f945c238e764f791a46
Carlos Fente Ortiz <cfenteor@emeal.nttdata.com>
2024-04-27 07:57:14 +0200

    FEAT: configure entry point for Heroes module

--- src/app/modules/heroes/heroes.module.ts
+++ src/app/modules/heroes/heroes.module.ts
@@ -5,6 +5,7 @@ import { HeroesListComponent } from './components/heroes-list/heroes-list.compon
 import { HeroesFilterComponent } from './components/heroes-filter/heroes-filter.component';
 import { HeroesFormComponent } from './components/heroes-form/heroes-form.component';
 import { HeroesItemComponent } from './components/heroes-item/heroes-item.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -17,7 +18,10 @@ import { HeroesItemComponent } from './components/heroes-item/heroes-item.compon
     HeroesListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([
+      {path: '', component: HeroesListComponent}
+    ])
   ]
 })
 export class HeroesModule { }
