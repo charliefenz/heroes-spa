ab171a22ad5779df29c08463be80fc8535ec9af8
Carlos Fente Ortiz <cfenteor@emeal.nttdata.com>
2024-04-27 07:29:14 +0200

    FEAT: create Heroes components

---
+++ src/app/modules/heroes/components/heroes-actions/heroes-actions.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-actions',
+  standalone: true,
+  imports: [],
+  templateUrl: './heroes-actions.component.html',
+  styleUrl: './heroes-actions.component.css'
+})
+export class HeroesActionsComponent {
+
+}
---
+++ src/app/modules/heroes/components/heroes-form/heroes-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './heroes-form.component.html',
+  styleUrl: './heroes-form.component.css'
+})
+export class HeroesFormComponent {
+
+}
