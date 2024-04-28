8afac76cfbba59a9329ace2321aefdf3dda891f4
Carlos Fente Ortiz <cfenteor@emeal.nttdata.com>
2024-04-28 19:26:44 +0200

    FEAT: create unit test for a method that returns the list of heroes

--- src/app/mock-API/mock-api.service.spec.ts
+++ src/app/mock-API/mock-api.service.spec.ts
@@ -1,16 +1,42 @@
 import { TestBed } from '@angular/core/testing';
-
 import { MockApiService } from './mock-api.service';
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
+const testData = {
+  name: jasmine.any(String),
+  id: jasmine.any(Number),
+  age: jasmine.any(Number),
+  isActive: jasmine.any(Boolean),
+  image: jasmine.any(Boolean),
+  superpowers: jasmine.arrayContaining([jasmine.any(String)]),
+};
 
 describe('MockApiService', () => {
   let service: MockApiService;
 
-  beforeEach(() => {
+  beforeAll(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(MockApiService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('service', () => {
+    it('should be created', () => {
+      expect(service).toBeTruthy();
+    });
+  });
+
+  describe('getHeroes', () => {
+    it('should return the heroes with the Hero Interface', (done: DoneFn) => {
+      let heroes$;
+
+      heroes$ = service.getHeroes();
+      heroes$.subscribe((response) => {
+        expect(response.code).withContext('Status Code').toEqual(200);
+        expect(response.result).withContext('Response does not have the expected interface').toEqual(
+          jasmine.arrayContaining([jasmine.objectContaining(testData)])
+        );
+        done();
+      });      
+    });
   });
 });
