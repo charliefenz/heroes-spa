0e481f5b80f95436a34db12d3d186d108da3a768
Carlos Fente Ortiz <cfenteor@emeal.nttdata.com>
2024-05-01 22:29:12 +0200

    FEAT: implement configuration for unit test of getHeroes request

--- src/app/modules/heroes/services/heroes.service.spec.ts
+++ src/app/modules/heroes/services/heroes.service.spec.ts
@@ -1,16 +1,27 @@
 import { TestBed } from '@angular/core/testing';
-
 import { HeroesService } from './heroes.service';
 
 describe('HeroesService', () => {
-  let service: HeroesService;
+  let heroesService: HeroesService;
+  let mockApiService: jasmine.SpyObj<MockApiService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(HeroesService);
+    const MOCK_API_SPY = jasmine.createSpyObj('MockApiService', ['getHeroes']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        HeroesService,
+        { provide: MockApiService, useValue: MOCK_API_SPY },
+      ],
+    });
+    heroesService = TestBed.inject(HeroesService);
+    mockApiService = TestBed.inject(
+      MockApiService
+    ) as jasmine.SpyObj<MockApiService>;
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(heroesService).toBeTruthy();
+  });
   });
 });
