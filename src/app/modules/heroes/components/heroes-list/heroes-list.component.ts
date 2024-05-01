4c22ad3a2f9ac6900bec53c7bded5d378176281b
Carlos Fente Ortiz <cfenteor@emeal.nttdata.com>
2024-05-01 17:21:10 +0200

    FEAT: inject heroesService in HeroesListComp

--- src/app/modules/heroes/components/heroes-list/heroes-list.component.ts
+++ src/app/modules/heroes/components/heroes-list/heroes-list.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroes-list',
   templateUrl: './heroes-list.component.html',
   styleUrl: './heroes-list.component.css'
 })
-export class HeroesListComponent {
+export class HeroesListComponent implements OnInit{  
+  constructor(private heroesService: HeroesService) {}
 
+  ngOnInit(): void {
+  }
+  }
 }
