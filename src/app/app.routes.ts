c2c1561e8803631e2a838b2aff52c645072537e7
Carlos Fente Ortiz <cfenteor@emeal.nttdata.com>
2024-05-01 08:59:38 +0200

    FEAT: set /heroes route as main entry of the app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {path: '', loadChildren: () => import('./modules/heroes/heroes.module').then(m => m.HeroesModule)}
+  {path: 'heroes', loadChildren: () => import('./modules/heroes/heroes.module').then(m => m.HeroesModule)},
+  {path: '', redirectTo: '/heroes', pathMatch: 'full'},
 ];
