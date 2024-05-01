1cc99e8275ea52555bd8acf4f89f40f108d3eec5
Carlos Fente Ortiz <cfenteor@emeal.nttdata.com>
2024-05-01 22:30:26 +0200

    FEAT: create unit tests for getHeroes request

--- src/app/modules/heroes/services/heroes.service.spec.ts
+++ src/app/modules/heroes/services/heroes.service.spec.ts
@@ -1,5 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 import { HeroesService } from './heroes.service';
+import { MockApiService } from '../../../mock-API/mock-api.service';
+import { of, throwError } from 'rxjs';
+import { Hero } from '../../../models/hero';
 
 describe('HeroesService', () => {
   let heroesService: HeroesService;
@@ -23,5 +26,52 @@ describe('HeroesService', () => {
   it('should be created', () => {
     expect(heroesService).toBeTruthy();
   });
+
+  describe('getHeroes', () => {
+    const MOCK_RESPONSE_OK = {
+      code: 200,
+      result: [
+        {
+          id: 1,
+          name: 'heroTest',
+          age: 30,
+          image: 'someUrl',
+          isActive: true,
+          superpowers: ['some', 'super', 'power'],
+        },
+        {
+          id: 2,
+          name: 'heroTest2',
+          age: 30,
+          image: 'someUrl',
+          isActive: true,
+          superpowers: ['some', 'super', 'power'],
+        },
+      ],
+    };
+    const ERROR_MESSAGE = 'Error fetching heroes';
+
+    it('should expose the observable containing heroes from the mock API', () => {
+      mockApiService.getHeroes.and.returnValue(of(MOCK_RESPONSE_OK));
+      heroesService.getHeroes().subscribe((heroesResponse) => {
+        let heroes = heroesResponse.result as Hero[];
+        expect(heroesResponse.code).toEqual(200);
+        expect(heroes).toEqual(MOCK_RESPONSE_OK.result);
+      });
+    });
+
+    it('should propagate the error when the API mock service fails unexpectedly', () => {
+      mockApiService.getHeroes.and.returnValue(throwError(() => new Error(ERROR_MESSAGE)));
+      spyOn(console, 'error');
+      heroesService.getHeroes().subscribe({
+        next: () => {
+          fail('Expected error but got a value')
+        },
+        error: (error) => {
+          expect(console.error).toHaveBeenCalledWith('Error fetching data for get heroes list', error)
+        }
+      })
+      expect(mockApiService.getHeroes).toHaveBeenCalled();
+    });
   });
 });
