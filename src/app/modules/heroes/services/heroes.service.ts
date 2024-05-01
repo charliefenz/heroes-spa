a6c594236ea63d529d31e0dc4c5631a91ebad1ac
Carlos Fente Ortiz <cfenteor@emeal.nttdata.com>
2024-05-01 17:19:05 +0200

    FEAT: implement getHeroes method

--- src/app/modules/heroes/services/heroes.service.ts
+++ src/app/modules/heroes/services/heroes.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { MockApiService } from '../../../mock-API/mock-api.service';
+import { Response } from '../../../models/response';
+import { Observable, catchError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,5 +9,16 @@ import { MockApiService } from '../../../mock-API/mock-api.service';
 export class HeroesService {
   
   constructor(private mockAPI: MockApiService) { 
+  }
+
+  getHeroes(): Observable<Response> {
+    // with pipe the observable is resent to any item injecting this service
+    // just like httpClient would do
+    return this.mockAPI.getHeroes().pipe(
+      catchError(error => {
+        console.error('Error fetching data for get heroes list', error);
+        throw error;
+      })
+    )
   }
 }
