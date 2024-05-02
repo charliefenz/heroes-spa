41769ada4e6ab62c657a15e1e21d9bc12e6635e4
Carlos Fente Ortiz <cfenteor@emeal.nttdata.com>
2024-05-02 22:50:07 +0200

    FEAT: change behavior of propagating error
     Now it catches the error and propagates a json 500 response

--- src/app/modules/heroes/services/heroes.service.ts
+++ src/app/modules/heroes/services/heroes.service.ts
@@ -1,23 +1,28 @@
 import { Injectable } from '@angular/core';
 import { MockApiService } from '../../../mock-API/mock-api.service';
 import { Response } from '../../../models/response';
-import { Observable, catchError } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
-  
+  private mockApiErrorResult = (method: string) : string => `Ha ocurrido un error en el método ${method} de la API`;
+  private mockApiErrorCode = 500;
+  private mockApiErrorResponse = {
+    code: this.mockApiErrorCode,
+    result: ''
+  }
+    
   constructor(private mockAPI: MockApiService) { 
   }
 
-  getHeroes(): Observable<Response> {
-    // with pipe the observable is resent to any item injecting this service
-    // just like httpClient would do
+  getHeroes(): Observable<Response> {  
     return this.mockAPI.getHeroes().pipe(
       catchError(error => {
-        console.error('Error fetching data for get heroes list', error);
-        throw error;
+        console.error(error);
+        this.mockApiErrorResponse.result = this.mockApiErrorResult('getHeroes');
+        return of(this.mockApiErrorResponse);
       })
     )
   }
