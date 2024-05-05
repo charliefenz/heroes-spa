7645302794e99046974b877f3e04cac9f809e334
Carlos Fente Ortiz <cfenteor@emeal.nttdata.com>
2024-05-05 14:45:05 +0200

    FEAT: organize and create unit tests according to component requirements

--- src/app/modules/heroes/components/hero-form/hero-form.component.spec.ts
+++ src/app/modules/heroes/components/hero-form/hero-form.component.spec.ts
@@ -36,18 +36,94 @@ describe('HeroFormComponent', () => {
     harness.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  describe('general Behavior', () => {
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
 
-  it('should navigate back to "heroes" route when button assigned to go back is clicked', fakeAsync(() => {
+    it('should display go-back button when loading', fakeAsync(() => {    
+    }));
+
+    it('should navigate back to "heroes" route when go-back button is clicked', fakeAsync(() => {    
+      // // const navigateSpy = spyOn(router, 'navigate');
+      // const button = fixture.debugElement.query(By.css('button#go-back-button'));
+  
+      // button.nativeElement.click();
+      // console.log(harness.routeNativeElement?.ATTRIBUTE_NODE);
+      // // tick(); 
+      // // expect(navigateSpy).toHaveBeenCalledWith(['../heroes'], {relativeTo: route});
+    }));
+  })
+
+  describe('Behavior when creating a hero', () =>{  
+    it('should load the form with empty values', fakeAsync(() => {
+    }));
+
+    it('should enable all form inputs upon loading', fakeAsync(() => {
+    }));
+
+    it('should not display edit button', fakeAsync(() => {
+    }));
+
+    it('should disable save button and cancel button upon loading', fakeAsync(() => {
+    }));
+
+    it('should enable save button and cancel button when the form detects interaction', fakeAsync(() => {
+    }));
+
+    it('should clear all inputs when cancel button is clicked', fakeAsync(() => {
+    }));
+
+    it('should disable save button and cancel button when cancel button is clicked', fakeAsync(() => {
+    }));
+  })
+
+  describe('Behavior when displaying a hero', () => {
+    it('should load the form with the correspondant values of the Hero', fakeAsync(() => {
+    }));
+
+    it('should disable all form inputs upon loading', fakeAsync(() => {
+    }));
+
+    it('should display edit button upon loading', fakeAsync(() => {
+    }));
+
+    it('should not display save button and cancel button upon loading', fakeAsync(() => {
+    }));
+
+    it('should display save button and cancel button when the edit button is clicked', fakeAsync(() => {
+    }));
+
+    it('should enable all form inputs when the edit button is clicked', fakeAsync(() => {
+    }));
+
+    it('should display original values when cancel button is clicked', fakeAsync(() => {
+    }));
+
+    it('should disable all form inputs when cancel button is clicked', fakeAsync(() => {
+    }));
     
-    // const navigateSpy = spyOn(router, 'navigate');
-    const button = fixture.debugElement.query(By.css('button#go-back-button'));
+    it('should not display save button and cancel button after cancel button is clicked', fakeAsync(() => {
+    }));
+  })
 
-    button.nativeElement.click();
-    console.log(harness.routeNativeElement?.ATTRIBUTE_NODE);
-    // tick(); 
-    // expect(navigateSpy).toHaveBeenCalledWith(['../heroes'], {relativeTo: route});
-  }));
+  describe('Behavior when saving', () => {
+    it('should send the input values when save button is clicked', fakeAsync(() => {
+    }));
+
+    it('it should provide with a confirmation notification if saving was a succces', fakeAsync(() => {
+    }));
+
+    it('it should provide with a error notification if saving was not a succces', fakeAsync(() => {
+    }));
+
+    it('it should remain in the view when saving was a success and the operation performed was an edit', fakeAsync(() => {
+    }));
+
+    it('it should redirect to "heroes" route when saving was a success and the operation performed was the creation of a new hero', fakeAsync(() => {
+    }));
+
+    it('should display an error message next to each input if the format is empty or incorrect', fakeAsync(() => {
+    }));
+  })
 });
