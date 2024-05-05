d927a1e5b8c0dd8e1dffa6f0c15db0a73a62f2ec
Carlos Fente Ortiz <cfenteor@emeal.nttdata.com>
2024-05-06 00:20:06 +0200

    FEAT: create injection of hero to heroes-actions

--- src/app/modules/heroes/components/heroes-actions/heroes-actions.component.ts
+++ src/app/modules/heroes/components/heroes-actions/heroes-actions.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Hero } from '../../../../models/hero';
 
 @Component({
   selector: 'app-heroes-actions',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './heroes-actions.component.css'
 })
 export class HeroesActionsComponent {
+  @Input() hero: Hero | undefined;
 
 }
