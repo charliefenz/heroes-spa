974d129299a408c6c6a1c2364f62802ee593b532
Carlos Fente Ortiz <cfenteor@emeal.nttdata.com>
2024-05-06 17:59:54 +0200

    FEAT: create routing for clicking edit button

--- src/app/modules/heroes/components/heroes-actions/heroes-actions.component.ts
+++ src/app/modules/heroes/components/heroes-actions/heroes-actions.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Hero } from '../../../../models/hero';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes-actions',
@@ -9,4 +10,9 @@ import { Hero } from '../../../../models/hero';
 export class HeroesActionsComponent {
   @Input() hero: Hero | undefined;
 
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  navigateToHero(heroId: number | undefined) {
+    this.router.navigate(['hero', heroId], {relativeTo: this.route})
+  }
 }
