650001829bc4a07d03afa3f7e8842ba9a732b906
Carlos Fente Ortiz <cfenteor@emeal.nttdata.com>
2024-05-07 18:45:13 +0200

    FEAT: create initial component structure

---
+++ src/app/shared/components/actionable-pop-up/actionable-pop-up.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-actionable-pop-up',
+  standalone: true,
+  imports: [],
+  templateUrl: './actionable-pop-up.component.html',
+  styleUrl: './actionable-pop-up.component.css'
+})
+export class ActionablePopUpComponent {
+
+}
