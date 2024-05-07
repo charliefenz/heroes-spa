d06242201880e4402750e94d3cb14cc2f93e5e85
Carlos Fente Ortiz <cfenteor@emeal.nttdata.com>
2024-05-08 00:41:18 +0200

    FEAT: create deletion behavior

--- src/app/modules/heroes/components/heroes-actions/heroes-actions.component.ts
+++ src/app/modules/heroes/components/heroes-actions/heroes-actions.component.ts
@@ -9,10 +9,23 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class HeroesActionsComponent {
   @Input() heroId: number | undefined;
+  activateDeletePopUp = false;
+  deleteMessage = '¿Estás seguro que deseas eliminar el héroe?'
 
   constructor(private router: Router, private route: ActivatedRoute) {}
 
   navigateToHero(heroId: number | undefined) {
     this.router.navigate(['hero', heroId], {relativeTo: this.route})
+  }
+
+  handleDeleteAnswer(deletionConfirmed: boolean) {
+    this.activateDeletePopUp = false;
+    if (deletionConfirmed) {
+      this.deleteHero(this.heroId);
+    }
+  }
+
+  deleteHero(heroId: number | undefined) {
+    console.log('about to delete')
   }
 }
