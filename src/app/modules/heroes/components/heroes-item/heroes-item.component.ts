86f3b5e0a72fd36a2eb4fff09a833dc9214549f5
Carlos Fente Ortiz <cfenteor@emeal.nttdata.com>
2024-05-08 18:22:32 +0200

    FEAT: set heroItem to propagate delete behavior to heroe-list

--- src/app/modules/heroes/components/heroes-item/heroes-item.component.ts
+++ src/app/modules/heroes/components/heroes-item/heroes-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Hero } from '../../../../models/hero';
 
 @Component({
@@ -8,5 +8,5 @@ import { Hero } from '../../../../models/hero';
 })
 export class HeroesItemComponent {
   @Input() hero: Hero | undefined;
-
+  @Output() propagateDeletion: EventEmitter<number> = new EventEmitter();
 }
