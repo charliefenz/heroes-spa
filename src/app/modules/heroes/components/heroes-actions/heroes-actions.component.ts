ca04d239c9768277303dce635077f6cc14ce19d9
Carlos Fente Ortiz <cfenteor@emeal.nttdata.com>
2024-05-08 18:21:29 +0200

    FEAT: update deleteBehavior to propagate to heroe-item

--- src/app/modules/heroes/components/heroes-actions/heroes-actions.component.ts
+++ src/app/modules/heroes/components/heroes-actions/heroes-actions.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Hero } from '../../../../models/hero';
 import { ActivatedRoute, Router } from '@angular/router';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroes-actions',
@@ -9,23 +10,18 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class HeroesActionsComponent {
   @Input() heroId: number | undefined;
+  @Output() performDeletion: EventEmitter<number> = new EventEmitter();
   activateDeletePopUp = false;
   deleteMessage = '¿Estás seguro que deseas eliminar el héroe?'
 
-  constructor(private router: Router, private route: ActivatedRoute) {}
+  constructor(private router: Router, private route: ActivatedRoute, private heroesService: HeroesService) {}
 
   navigateToHero(heroId: number | undefined) {
     this.router.navigate(['hero', heroId], {relativeTo: this.route})
   }
 
-  handleDeleteAnswer(deletionConfirmed: boolean) {
+  handleDeleteAnswer(userWantsToDelete: boolean) {
+    if (userWantsToDelete && this.heroId) this.performDeletion.emit(this.heroId);
     this.activateDeletePopUp = false;
-    if (deletionConfirmed) {
-      this.deleteHero(this.heroId);
-    }
-  }
-
-  deleteHero(heroId: number | undefined) {
-    console.log('about to delete')
   }
 }
