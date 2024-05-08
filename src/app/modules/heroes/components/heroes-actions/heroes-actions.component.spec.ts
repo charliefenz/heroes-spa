bcb84ecc4db0af29f42a660d9c1d372222fad0b9
Carlos Fente Ortiz <cfenteor@emeal.nttdata.com>
2024-05-08 18:32:42 +0200

    FEAT: list unit tests for heroes-action

--- src/app/modules/heroes/components/heroes-actions/heroes-actions.component.spec.ts
+++ src/app/modules/heroes/components/heroes-actions/heroes-actions.component.spec.ts
@@ -20,4 +20,9 @@ describe('HeroesActionsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should navigate to "hero/heroId" route when clicking more-details button', () => {})
+  it('should activate deletion pop up and provide a message for it when clicking delete button', () => {})
+  it('should deactivate deletion pop up when clicking a yes or no option', () => {})
+  it('should emit heroId to hero-item component when "yes" is selected in deletion pop up', () => {})
 });
