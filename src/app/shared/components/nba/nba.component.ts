995cf4a5426eb434fb9a91a873f097de012a1a4c
Carlos Fente Ortiz <cfenteor@emeal.nttdata.com>
2024-05-09 23:00:20 +0200

    FEAT: create nba component

---
+++ src/app/shared/components/nba/nba.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nba',
+  standalone: true,
+  imports: [],
+  templateUrl: './nba.component.html',
+  styleUrl: './nba.component.css'
+})
+export class NbaComponent {
+
+}
