48decd15ce90ddadb17a5d2459776fa4eba34ba3
Carlos Fente Ortiz <cfenteor@emeal.nttdata.com>
2024-05-11 18:18:57 +0200

    REFACTOR: unused imports in actionablePopUp

--- src/app/shared/components/actionable-pop-up/actionable-pop-up.component.ts
+++ src/app/shared/components/actionable-pop-up/actionable-pop-up.component.ts
@@ -2,29 +2,21 @@ import {Component, Inject} from '@angular/core';
 import {
   MAT_DIALOG_DATA,
   MatDialogRef,
-  MatDialogTitle,
   MatDialogContent,
   MatDialogActions,
   MatDialogClose,
 } from '@angular/material/dialog';
 import {MatButtonModule} from '@angular/material/button';
-import {FormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-actionable-pop-up',
   templateUrl: './actionable-pop-up.component.html',
   standalone: true,
   imports: [
-    MatFormFieldModule,
-    MatInputModule,
-    FormsModule,
     MatButtonModule,
-    MatDialogTitle,
     MatDialogContent,
     MatDialogActions,
-    MatDialogClose,
+    MatDialogClose
   ]
 })
 export class ActionablePopUpComponent {
