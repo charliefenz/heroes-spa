a0a0c94567ae3486b87dfb435d4919900040b89f
Carlos Fente Ortiz <cfenteor@emeal.nttdata.com>
2024-05-11 16:28:50 +0200

    FEAT: import spinner component

--- src/app/shared/components/loader/loader.component.ts
+++ src/app/shared/components/loader/loader.component.ts
@@ -1,10 +1,11 @@
 import { NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-loader',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass, MatProgressSpinnerModule],
   templateUrl: './loader.component.html',
   styleUrl: './loader.component.scss'
 })
