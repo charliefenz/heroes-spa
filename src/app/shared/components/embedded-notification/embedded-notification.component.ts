6d852283afb7291e62bbe0f767d10d088bdeb0ce
Carlos Fente Ortiz <cfenteor@emeal.nttdata.com>
2024-05-11 20:30:56 +0200

    FEAT: implement initial info icon

--- src/app/shared/components/embedded-notification/embedded-notification.component.ts
+++ src/app/shared/components/embedded-notification/embedded-notification.component.ts
@@ -1,11 +1,12 @@
 import { NgClass } from '@angular/common';
 import { Component, Input} from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-embedded-notification',
   standalone: true,
-  imports: [NgClass, MatCardModule],
+  imports: [NgClass, MatCardModule, MatIconModule],
   templateUrl: './embedded-notification.component.html',
   styleUrl: './embedded-notification.component.scss'
 })
