7d1eab6f7ee1057cbc5ffd800c1804f2ab9ffab4
Carlos Fente Ortiz <cfenteor@emeal.nttdata.com>
2024-05-13 15:09:55 +0200

    FEAT: create ad-hoc component image-input

---
+++ src/app/modules/heroes/components/image-input-dialog/image-input-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-input-dialog',
+  templateUrl: './image-input-dialog.component.html',
+  styleUrl: './image-input-dialog.component.scss'
+})
+export class ImageInputDialogComponent {
+
+}
