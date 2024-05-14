89e9bfe5e9a359497f5ac7587590df4d7644bf2b
Carlos Fente Ortiz <cfenteor@emeal.nttdata.com>
2024-05-15 00:26:00 +0200

    FEAT: create behavior for 404 page

--- src/app/shared/components/not-found-page/not-found-page.component.ts
+++ src/app/shared/components/not-found-page/not-found-page.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { EmbeddedNotificationComponent } from '../embedded-notification/embedded-notification.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-not-found-page',
   standalone: true,
-  imports: [],
+  imports: [EmbeddedNotificationComponent, MatButtonModule],
   templateUrl: './not-found-page.component.html',
   styleUrl: './not-found-page.component.scss'
 })
 export class NotFoundPageComponent {
+  errorMessage = "¡Oops, no encontramos lo que buscabas!";
 
+  constructor(private router: Router) {}
+
+  navigateToHome() {
+    this.router.navigate(['/heroes'])
+  }
 }
